10e2c87f54456645a55958db58382febe508b9f8
snahmd <sanahmed7@gmail.com>
2024-10-10 02:00:33 +0200

    Update section height and styling, adjust positioning of elements

--- scroll.js
+++ scroll.js
@@ -5,11 +5,15 @@ const scrollableSection = document.querySelector("#scrollsection");
 const scroll = () => {
   //const distance = window.scrollY;
   // calculate how much scrolled in the section
+  const pathLength = path.getTotalLength();
+
   const distance = window.scrollY - scrollableSection.offsetTop / 2;
   if (
     distance >
     scrollableSection.offsetHeight - scrollableSection.offsetTop / 2
   ) {
+    path.style.strokeDasharray = `${pathLength}`;
+    path.style.strokeDashoffset = `0`;
     return;
   }
   if (distance < 0) {
@@ -17,11 +21,12 @@ const scroll = () => {
   }
   const percentage = distance / scrollableSection.offsetHeight;
 
-  const pathLength = path.getTotalLength();
-
   path.style.strokeDasharray = `${pathLength}`;
-  path.style.strokeDashoffset = `${pathLength - pathLength * (percentage * 3)}`;
+  path.style.strokeDashoffset = `${Math.max(
+    0,
+    pathLength - pathLength * (percentage * 2)
+  )}`;
 };
 
-scroll();
+//scroll();
 window.addEventListener("scroll", scroll);
